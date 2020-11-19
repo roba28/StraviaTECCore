4d47d6a7e99bf1fe61490b1fc3113aa2895b2e75
Jonathan García Ugalde <jonagar@hotmail.com>
2020-11-19 13:00:35 -0600

    cambios en AmigoController

---
+++ StraviaTECCore/Controllers/AmigosController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StraviaTECCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AmigosController : ControllerBase
+    {
+
+        [HttpPost]
+
+        public IActionResult addFriend(Models.Amigos nuevoAmigo)
+        {
+
+            using (Straviatec_DBContext db = new Straviatec_DBContext())
+            {
+                try
+                {// se inicializa el  objeto actividad
+                    Actividad actividad1 = new Actividad(nuevoAmigo.UsuarioId, nuevoAmigo.Tiempo, nuevoAmigo.Distancia, nuevoAmigo.Fecha, nuevoAmigo.Hora, nuevoAmigo.InicioRecorrido, nuevoAmigo.Finrecorrido, nuevoAmigo.ActividadId);//, actividad.Usuario);   
+                                                                                                                                                                                                                                // se inserta en la base de datos.
+
+
+                    db.Actividad.Add(actividad1);
+                    db.SaveChanges();
+
+                    return Ok();
+                }
+                catch
+                {
+                    return NotFound();
+                }
+            }
+        }
+    }
+}

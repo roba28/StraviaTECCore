c789c85ad87e1cd524f26ba940f1d7154b4e78ab
Ronny <ronny199528@gmail.com>
2020-11-18 19:48:55 -0600

    Agregar archivos de proyecto.

---
+++ StraviaTECCore/Models/Gestionretos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StraviaTECCore.Models
+{
+    public partial class Gestionretos
+    {
+        public int RetoId { get; set; }
+        public int OrganizadorId { get; set; }
+        public int CategoriaId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime? Periodo { get; set; }
+
+        public Categoria Categoria { get; set; }
+        public Usuarios Organizador { get; set; }
+    }
+}
